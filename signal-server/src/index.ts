80b23701493110f2b23ad229ad4e9bb0bf87f15f
hippo <53283780+HippoCheer@users.noreply.github.com>
2021-08-27 17:52:50 +0800

    add

---
+++ signal-server/src/index.ts
@@ -0,0 +1,209 @@
+import WebSocket, { Server } from 'ws'
+
+class User {
+  readonly name: string;
+  private _connection: WebSocket;
+
+  get connection() {
+    return this._connection
+  }
+
+  constructor(name: string, connection: WebSocket) {
+    if (!name) {
+      throw new Error('invalid username')
+    }
+    this.name = name
+    this._connection = connection
+  }
+  changeConnection(connection: WebSocket) {
+    this._connection = connection
+  }
+}
+
+class Channel {
+  readonly name: string;
+  users: User[] = [];
+  constructor(name: string, users?: User[]) {
+    if (!name) {
+      throw new Error('invalid channel name')
+    }
+    this.name = name
+    if (users) {
+      this.users = users
+    }
+  }
+  addUser(user: User) {
+    const [existUser] = this.users.filter(({ name }) => name === user.name)
+    if (existUser) {
+      existUser.changeConnection(user.connection)
+    } else {
+      this.users.push(user)
+    }
+    return this
+  }
+  removeUser(username: string) {
+    this.users = this.users.filter(({ name }) => name !== username)
+  }
+}
+
+interface JoinRequest {
+  action: 'join',
+  data: {
+    channel: string,
+    username: string
+  }
+}
+
+interface LeaveRequest {
+  action: 'leave',
+  data: {
+    channel: string,
+    username: string
+  }
+}
+
+interface JoinResponse {
+  action: 'user-join',
+  data: string
+}
+
+interface LeaveResponse {
+  action: 'user-leave',
+  data: string
+}
+
+interface CallRequest {
+  action: 'call',
+  data: {
+    offer: any,
+    to: string,
+    channel: string,
+    from: string
+  }
+}
+
+interface CallResponse {
+  action: 'call-response',
+  data: {
+    offer: any,
+    from: string
+  }
+}
+
+interface AnswerRequest {
+  action: 'answer',
+  data: {
+    answer: any,
+    to: string,
+    channel: string,
+    from: string
+  }
+}
+
+interface AnswerResponse {
+  action: 'answer-response',
+  data: {
+    answer: any,
+    from: string
+  }
+}
+
+type WSMessage = JoinRequest | LeaveRequest | CallRequest | AnswerRequest
+
+const channels: Channel[] = []
+
+const wss = new Server({
+  port: 9000
+})
+
+wss.on('connection', (ws) => {
+  ws.on('message', (messageStr) => {
+    const message: WSMessage = JSON.parse(messageStr as string)
+    if (message.action === 'join') {
+      joinChannelHandler(message, ws)
+    }
+    if (message.action === 'leave') {
+      leaveChannelHandler(message, ws)
+    }
+    if (message.action === 'call') {
+      callChannelHandler(message)
+    }
+    if (message.action === 'answer') {
+      answerChannelHandler(message)
+    }
+  })
+})
+
+function joinChannelHandler(message: JoinRequest, ws: WebSocket) {
+  const { username, channel } = message.data
+  const [existChannel] = channels.filter(({ name }) => name === channel)
+  if (existChannel) {
+    const { users } = existChannel
+    const [existUser] = users.filter(({ name }) => name === username)
+    if (existUser) {
+      existUser.changeConnection(ws)
+    } else {
+      const joinResponse: JoinResponse = {
+        action: 'user-join',
+        data: username
+      }
+      existChannel.users.forEach((user) => {
+        user.connection.send(JSON.stringify(joinResponse))
+      })
+      existChannel.addUser(new User(username, ws))
+    }
+  } else {
+    channels.push(new Channel(channel).addUser(new User(username, ws)))
+  }
+}
+
+function leaveChannelHandler(message: LeaveRequest, ws: WebSocket) {
+  const { username, channel } = message.data
+  const [targetChannel] = channels.filter(({ name }) => name === channel)
+  if (targetChannel) {
+    const leaveResponse: LeaveResponse = {
+      action: 'user-leave',
+      data: username
+    }
+    targetChannel.removeUser(username)
+    targetChannel.users.forEach((user) => {
+      user.connection.send(JSON.stringify(leaveResponse))
+    })
+  }
+}
+
+function callChannelHandler(message: CallRequest) {
+  const { to, offer, channel, from } = message.data
+  const [targetChannel] = channels.filter(({ name }) => name === channel)
+  if (targetChannel) {
+    const [targetUser] = targetChannel.users.filter(({ name }) => name === to)
+    if (targetUser) {
+      const callResponse: CallResponse = {
+        action: 'call-response',
+        data: {
+          offer,
+          from
+        }
+      }
+      targetUser.connection.send(JSON.stringify(callResponse))
+    }
+  }
+}
+
+function answerChannelHandler(message: AnswerRequest) {
+  const { to, answer, channel, from } = message.data
+  const [targetChannel] = channels.filter(({ name }) => name === channel)
+  if (targetChannel) {
+    const [targetUser] = targetChannel.users.filter(({ name }) => name === to)
+    if (targetUser) {
+      const answerResponse: AnswerResponse = {
+        action: 'answer-response',
+        data: {
+          answer,
+          from
+        }
+      }
+      targetUser.connection.send(JSON.stringify(answerResponse))
+    }
+  }
+}
